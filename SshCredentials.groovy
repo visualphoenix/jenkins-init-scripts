a83aa48f9ad9672f7aa56dbe62af973d1eac971e
Raymond Barbiero <raymond.barbiero.dev@gmail.com>
2016-10-04 22:19:55 -0400

    default to using the ssh keys in the user's home directory

--- SshCredentials.groovy
+++ SshCredentials.groovy
@@ -1,5 +1,5 @@
 // Configure global ssh credentials
-def ssh_credential = { instance, id, username, description, privateKeySource=null, passphrase='' ->
+def ssh_credential = { instance, id, username, description, privateKeySource=new com.cloudbees.jenkins.plugins.sshcredentials.impl.BasicSSHUserPrivateKey.UsersPrivateKeySource(), passphrase='' ->
   // Retrieve the Global credential store
   def domain = com.cloudbees.plugins.credentials.domains.Domain.global()
   def store = instance.getExtensionList('com.cloudbees.plugins.credentials.SystemCredentialsProvider')[0].getStore()
